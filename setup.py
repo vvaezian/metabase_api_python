5ad4686e914a89734498446ced370cd89fe2c8ff
Vahid <vahid.vaezian@gmail.com>
2020-01-20 17:50:53 -0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="metabase-api",
+    version="0.1",
+    author="Vahid Vaezian",
+    author_email="vahid.vaezian@gmail.com",
+    description="A Python Wrapper for Metabase API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vvaezian/metabase_api_python",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
