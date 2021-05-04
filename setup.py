640fe70feb360e13aeff96c82ee1b51ce7e404a0
Vahid <vahid.vaezian@gmail.com>
2021-05-03 22:13:59 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="metabase-api",
-    version="0.2.10",
+    version="0.2.11",
     author="Vahid Vaezian",
     author_email="vahid.vaezian@gmail.com",
     description="A Python Wrapper for Metabase API",
