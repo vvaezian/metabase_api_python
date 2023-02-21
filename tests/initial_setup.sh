9c4f97d02b4e95632511322127c72cd0843ca0fb
Vahid Vaezian <vahid.vaezian@gmail.com>
2023-02-21 11:21:39 -0800

    local test

---
+++ tests/initial_setup.sh
@@ -0,0 +1,179 @@
+# This file does some initial setup so we can run tests on a local Metabase:
+# - Downloads the Metabase jar file
+# - Runs it
+# - Creates an admin user (email: abc.xyz@gmail.com, password: xzy12345) and does the initial setup of Metabase
+# - Creates some collections/cards/dashboards which will be used when running the tests
+
+
+while getopts 'v:' flag; do
+  case "${flag}" in
+    v) MB_VERSION="${OPTARG}" ;;
+    *) echo 'Accepted flags: -v'
+       exit 1 ;;
+  esac
+done
+
+if [[ $MB_VERSION = '' ]]
+then
+    echo 'Please provide the Metabase version using -v flag.'
+    exit 1
+fi
+
+echo https://downloads.metabase.com/v$MB_VERSION/metabase.jar
+pwd
+ls -l
+#cp tests/data/test_db.sqlite plugins
+
+# wget https://downloads.metabase.com/v$MB_VERSION/metabase.jar
+# echo "starting metabase jar locally ..."
+# java -jar metabase.jar > logs 2>&1 &
+
+# echo "waiting 60 seconds for the initialization to complete ..."
+# sleep 60
+
+# success='False'
+# grep -q "Metabase Initialization COMPLETE" logs
+# if [[ $? -eq 0 ]] 
+# then 
+#     echo 'success!'
+#     success='True'
+# else
+#     echo "Waiting an extra 60 seconds for the initialization to complete"
+#     sleep 60
+#     grep -q "Metabase Initialization COMPLETE" logs
+#     if [[ $? -eq 0 ]] 
+#     then 
+#         echo 'success!'
+#         success='True'
+#     else
+#         echo 'failure!'
+#     fi      
+# fi
+
+# if [[ $success = 'False' ]]
+# then
+#     exit 1
+# fi
+
+# echo "getting the seup token ..."
+# setup_token=$(curl -X GET http://localhost:3000/api/session/properties | perl -pe 's/.*"setup-token":"(.*?)".*/\1/')
+# echo "initial setup and getting session_id ..."
+# session_id=$(curl -X POST -H "Content-Type: application/json" -d '{ "token": "'$setup_token'", "user": {"first_name": "abc", "last_name": "xyz", "email": "abc.xyz@gmail.com", "password": "xzy12345"},"prefs": {"allow_tracking": true, "site_name": "test_site"}}' http://localhost:3000/api/setup | perl -pe 's/^.......(.*)..$/\1/')
+# echo $session_id
+
+# echo "creaing base collections which will be used during the test ..."
+# curl -X POST -H "Content-Type: application/json" -H "X-Metabase-Session:$session_id" -d '{"name":"test_collection", "color":"#509EE3"}' http://localhost:3000/api/collection  # id of the created collection is 2 because id 1 is reserved for the personal collection of admin
+# curl -X POST -H "Content-Type: application/json" -H "X-Metabase-Session:$session_id" -d '{"name":"test_collection_dup", "parent_id":2, "color":"#509EE3"}' http://localhost:3000/api/collection  # collection_id: 3
+# curl -X POST -H "Content-Type: application/json" -H "X-Metabase-Session:$session_id" -d '{"name":"test_collection_dup", "parent_id":2, "color":"#509EE3"}' http://localhost:3000/api/collection  # collection_id: 4
+
+# echo "downloading the test_db (SQLite)"  # for editing the SQLite db: https://sqliteviewer.flowsoft7.com/
+# wget -P plugins/ https://github.com/vvaezian/metabase_api_python/raw/master/tests/data/test_db.sqlite
+
+# echo "Connecting to test_db ..."
+# curl -X POST -H "Content-Type: application/json" -H "X-Metabase-Session:$session_id" -d '{"engine":"sqlite","name":"test_db","details":{"db":"plugins/test_db.sqlite","advanced-options":false},"is_full_sync":true}' http://localhost:3000/api/database  # id of the created db connection is 2 because 1 is used for sample database
+
+# # echo "creating base cards which will be used during the test ..."
+# json='{
+#     "name": "test_card",
+#     "display": "table",
+#     "dataset_query": {
+#         "database": 2,
+#         "query": { "source-table": 9 },
+#         "type": "query"
+#     },
+#     "visualization_settings": {},
+#     "collection_id": 2
+# }'
+# echo "$json" | curl -X POST http://localhost:3000/api/card -H "Content-Type: application/json" -H "X-Metabase-Session:$session_id" -d @- 
+
+# json='{
+#     "name":"test_card_2",
+#     "dataset_query":{
+#         "type":"native",
+#         "native":{
+#             "query":"select *\nfrom test_table\nwhere 1 = 1 \n[[ and {{test_filter}} ]]\n",
+#             "template-tags":{
+#                 "test_filter":{
+#                     "name":"test_filter",
+#                     "display-name":"Test filter",
+#                     "type":"dimension",
+#                     "dimension":["field",72,null],
+#                     "widget-type":"string/=",
+#                     "default":null,
+#                     "id":"810912da-ead5-c87e-de32-6dc5723b9067"
+#                 }
+#             }
+#         }
+#         ,"database":2
+#     },
+#     "display":"table",
+#     "visualization_settings":{},
+#     "parameters":[{
+#         "type":"string/=",
+#         "target":["dimension",["template-tag","test_filter"]],
+#         "name":"Test filter",
+#         "slug":"test_filter",
+#         "default":null,
+#         "id":"810912da-ead5-c87e-de32-6dc5723b9067"
+#     }],
+#     "collection_id":2
+# }'
+# echo "$json" | curl -X POST http://localhost:3000/api/card -H "Content-Type: application/json" -H "X-Metabase-Session:$session_id" -d @- 
+
+# json='{
+#     "name":"test_card_3",
+#     "dataset_query":{
+#         "type":"query",
+#         "query":{
+#             "source-table":9,
+#             "filter":["=",["field",72,null],"row1 cell1","row3 cell1"]},
+#             "database":2
+#         },
+#         "display":"table",
+#         "visualization_settings":{},
+#         "collection_id":2
+# }'
+# echo "$json" | curl -X POST http://localhost:3000/api/card -H "Content-Type: application/json" -H "X-Metabase-Session:$session_id" -d @- 
+
+# json='{
+#     "name":"test_card_4",
+#     "dataset":false,
+#     "dataset_query":{
+#         "database":2,
+#         "query":{
+#             "source-table":9,
+#             "aggregation":[["avg",["field",73,null]]],
+#             "breakout":[["field",72,null]],
+#             "order-by":[["desc",["aggregation",0,null]]]
+#             },
+#         "type":"query"
+#     },
+#     "display":"bar",
+#     "visualization_settings":{"table.pivot":false,"graph.dimensions":["col1"],"graph.metrics":["avg"]},
+#     "collection_id":2
+# }'
+# echo "$json" | curl -X POST http://localhost:3000/api/card -H "Content-Type: application/json" -H "X-Metabase-Session:$session_id" -d @- 
+
+# # create a test dashboard
+# curl -X POST http://localhost:3000/api/dashboard -H "Content-Type: application/json" -H "X-Metabase-Session:$session_id" -d '{"collection_id":2,"name":"test_dashboard"}'
+
+# # add the test_card to the dashboard
+# curl -X POST http://localhost:3000/api/dashboard/1/cards -H "Content-Type: application/json" -H "X-Metabase-Session:$session_id" -d  '{"cardId":1}'
+# json='{
+#     "cards":[{
+#             "card_id":1,
+#             "row":0,
+#             "col":0,
+#             "size_x":4,
+#             "size_y":5,
+#             "series":[],
+#             "visualization_settings":{},
+#             "parameter_mappings":[]
+#     }]
+# }'
+# echo "$json" | curl -X PUT http://localhost:3000/api/dashboard/1/cards -H "Content-Type: application/json" -H "X-Metabase-Session:$session_id" -d @-
+
+# # diable friendly table and field names
+# curl -X PUT http://localhost:3000/api/setting/humanization-strategy -H "Content-Type: application/json" -H "X-Metabase-Session:$session_id" -d '{"value":"none"}'
+
+##session_id='69afe488-8037-47ec-b437-c6136d6d32c8'
--- tests/test_metabase_api.py
+++ tests/test_metabase_api.py
@@ -1,14 +1,9 @@
 from metabase_api.metabase_api import Metabase_API
 import datetime
-import os
 import unittest
 
 
-LOGIN_DOMAIN = os.environ.get("LOGIN_DOMAIN")
-LOGIN_EMAIL = os.environ.get("LOGIN_USERNAME")
-LOGIN_PASSWORD = os.environ.get("LOGIN_PASSWORD")
-
-mb = Metabase_API(LOGIN_DOMAIN, LOGIN_EMAIL, LOGIN_PASSWORD)
+mb = Metabase_API('http://localhost:3000', 'abc.xyz@gmail.com', 'xzy12345')
 
 
 class Metabase_API_Test(unittest.TestCase):
@@ -37,84 +32,71 @@ class Metabase_API_Test(unittest.TestCase):
   
   def test_get_item_info(self):
     # database
-    res = mb.get_item_info('database', 5)
+    res = mb.get_item_info('database', 2)
     self.assertEqual(res['name'], 'test_db')
-    self.assertEqual(res['id'], 5)
+    self.assertEqual(res['id'], 2)
 
     # table
-    res = mb.get_item_info('table', 101)
+    res = mb.get_item_info('table', 9)
     self.assertEqual(res['name'], 'test_table')
-    self.assertEqual(res['id'], 101)
+    self.assertEqual(res['id'], 9)
 
     # card
-    res = mb.get_item_info('card', 166)
+    res = mb.get_item_info('card', 1)
     self.assertEqual(res['name'], 'test_card')
-    self.assertEqual(res['id'], 166)
+    self.assertEqual(res['id'], 1)
 
     # collection
-    res = mb.get_item_info('collection', 28)
+    res = mb.get_item_info('collection', 2)
     self.assertEqual(res['name'], 'test_collection')
-    self.assertEqual(res['id'], 28)
+    self.assertEqual(res['id'], 2)
 
     # dashboard
-    res = mb.get_item_info('dashboard', 35)
+    res = mb.get_item_info('dashboard', 1)
     self.assertEqual(res['name'], 'test_dashboard')
-    self.assertEqual(res['id'], 35)
-
-    # segment'
-    res = mb.get_item_info('segment', 18)
-    self.assertEqual(res['name'], 'test_segment')
-    self.assertEqual(res['id'], 18)
-
-    # pulse
+    self.assertEqual(res['id'], 1)
 
 
 
   def test_get_item_name(self):
     # database
-    db_id = mb.get_item_name('database', 5)
-    self.assertEqual(db_id, 'test_db')
+    db_name = mb.get_item_name('database', 2)
+    self.assertEqual(db_name, 'test_db')
 
     # table
-    table_id = mb.get_item_name('table', 101)
-    self.assertEqual(table_id, 'test_table')
+    table_name = mb.get_item_name('table', 9)
+    self.assertEqual(table_name, 'test_table')
 
     # card
-    card_id = mb.get_item_name('card', 166)
-    self.assertEqual(card_id, 'test_card')
+    card_name = mb.get_item_name('card', 1)
+    self.assertEqual(card_name, 'test_card')
 
     # collection
-    collection_id = mb.get_item_name('collection', 28)
-    self.assertEqual(collection_id, 'test_collection')
+    collection_name = mb.get_item_name('collection', 2)
+    self.assertEqual(collection_name, 'test_collection')
 
     # dashboard
-    dashboard_id = mb.get_item_name('dashboard', 35)
-    self.assertEqual(dashboard_id, 'test_dashboard')
-
-    # segment'
-    segment_id = mb.get_item_name('segment', 18)
-    self.assertEqual(segment_id, 'test_segment')
-
-    # pulse
+    dashboard_name = mb.get_item_name('dashboard', 1)
+    self.assertEqual(dashboard_name, 'test_dashboard')
 
 
 
   def test_get_item_id(self):
     # database
-    db_name = mb.get_item_id('database', 'test_db')
-    self.assertEqual(db_name, 5)
+    db_id = mb.get_item_id('database', 'test_db')
+    self.assertEqual(db_id, 2)
 
     # table
-    table_name = mb.get_item_id('table', 'test_table')
-    self.assertEqual(table_name, 101)
+    table_id = mb.get_item_id('table', 'test_table')
+    self.assertEqual(table_id, 9)
 
     # card
-    card_name = mb.get_item_id('card', 'test_card')
-    self.assertEqual(card_name, 166)
+    card_id = mb.get_item_id('card', 'test_card')
+    self.assertEqual(card_id, 1)
 
     # collection
-    collection_name = mb.get_item_id('collection', 'test_collection')
-    self.assertEqual(collection_name, 28)
+    collection_id = mb.get_item_id('collection', 'test_collection')
+    self.assertEqual(collection_id, 2)
 
     with self.assertRaises(ValueError) as error:
       mb.get_item_id('collection', 'test_collection_dup')  
@@ -125,41 +107,29 @@ class Metabase_API_Test(unittest.TestCase):
     self.assertEqual(str(error.exception), 'There is no collection with the name "xyz"')
 
     # dashboard
-    dashboard_name = mb.get_item_id('dashboard', 'test_dashboard')
-    self.assertEqual(dashboard_name, 35)
-
-    # pulse
-
-    # segment
-    segment_name = mb.get_item_id('segment', 'test_segment')
-    self.assertEqual(segment_name, 18)
+    dashboard_id = mb.get_item_id('dashboard', 'test_dashboard')
+    self.assertEqual(dashboard_id, 1)
 
 
 
   def test_get_db_id_from_table_id(self):
-    db_id = mb.get_db_id_from_table_id(101)
-    self.assertEqual(db_id, 5)
+    db_id = mb.get_db_id_from_table_id(9)
+    self.assertEqual(db_id, 2)
 
 
 
   def test_get_table_metadata(self):
-    table_info = mb.get_table_metadata(table_id=101)
+    table_info = mb.get_table_metadata(table_id=9)
     self.assertEqual(table_info['fields'][0]['name'], 'col1')
 
 
 
   def test_get_columns_name_id(self):
-    name_id_mapping = mb.get_columns_name_id(table_id=101)
-    self.assertEqual(name_id_mapping['col1'], 490)
+    name_id_mapping = mb.get_columns_name_id(table_id=9)
+    self.assertEqual(name_id_mapping['col1'], 72)
 
-    id_name_mapping = mb.get_columns_name_id(table_id=101, column_id_name=True)
-    self.assertEqual(id_name_mapping[490], 'col1')
-
-
-
-  def test_friendly_names_is_disabled(self):
-    res = mb.friendly_names_is_disabled()
-    self.assertEqual(res, True)
+    id_name_mapping = mb.get_columns_name_id(table_id=9, column_id_name=True)
+    self.assertEqual(id_name_mapping[72], 'col1')
 
 
 
@@ -167,18 +137,18 @@ class Metabase_API_Test(unittest.TestCase):
 
   def test_create_card(self):
     t = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    res1 = mb.create_card(card_name=f'test_create_card_{t}', table_name='test_table', collection_id=28, return_card=True)
+    res1 = mb.create_card(card_name=f'test_create_card_{t}', table_name='test_table', collection_id=2, return_card=True)
 
     t = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     card_info = {
       'name': f'test_create_card_json1_{t}',
       'display': 'table',
       'dataset_query': {
-        'database': 5,
+        'database': 2,
         'native': { 'query': 'select * from test_table' },
         'type': 'native' 
       },
-      'collection_id':28
+      'collection_id':2
     }
     res2 = mb.create_card(custom_json=card_info, return_card=True)
     Metabase_API_Test.cleanup_objects['card'].append(res2['id'])
@@ -188,12 +158,12 @@ class Metabase_API_Test(unittest.TestCase):
       'name': f'test_create_card_json2_{t}',
       'display': 'table',
       'dataset_query': {
-        'database': 5,
+        'database': 2,
         'native': { 'query': 'select * from test_table' },
         'type': 'native' 
       }
     }
-    res3 = mb.create_card(custom_json=card_info, collection_id=28, return_card=True)
+    res3 = mb.create_card(custom_json=card_info, collection_id=2, return_card=True)
     Metabase_API_Test.cleanup_objects['card'].append(res3['id'])
 
     t = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
@@ -201,18 +171,18 @@ class Metabase_API_Test(unittest.TestCase):
       'name': f'test_create_card_json3_{t}',
       #'display': 'table',
       'dataset_query': {
-        'database': 5,
+        'database': 2,
         'native': { 'query': 'select * from test_table' },
         'type': 'native' 
       }
     }
     with self.assertRaises(ValueError) as error:
-      mb.create_card(custom_json=card_info, collection_id=28)
+      mb.create_card(custom_json=card_info, collection_id=2)
     self.assertEqual(str(error.exception), 'Either the name or id of the table must be provided.')
 
     # check to make sure the cards were created in the right collection
     collection_IDs_of_created_cards = { i['collection_id'] for i in [res1, res2, res3] }
-    self.assertEqual(collection_IDs_of_created_cards, set({28}))
+    self.assertEqual(collection_IDs_of_created_cards, set({2}))
 
     # add id of the created cards to the cleaup list to be taken care of by the tearDown method
     Metabase_API_Test.cleanup_objects['card'].extend([ res1['id'], res3['id'], res3['id'] ])
@@ -221,33 +191,24 @@ class Metabase_API_Test(unittest.TestCase):
 
   def test_create_collection(self):
     t = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    res = mb.create_collection(f'test_create_collection {t}', parent_collection_id=28, return_results=True)
+    res = mb.create_collection(f'test_create_collection {t}', parent_collection_id=2, return_results=True)
 
     # check to make sure the collection was created in the right place
     res2 = mb.get('/api/collection/{}'.format(res['id']))
-    self.assertEqual(res2['parent_id'], 28)
+    self.assertEqual(res2['parent_id'], 2)
 
     # add to cleanup list
     Metabase_API_Test.cleanup_objects['collection'].append(res['id'])
 
 
 
-  def test_create_segment(self):
-    t = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    res = mb.create_segment(segment_name='test_create_segment_{}'.format(t), table_name='test_table', column_name='col2', column_values=[1, 2], return_segment=True)
-
-    # add to cleanup list
-    Metabase_API_Test.cleanup_objects['segment'].append(res['id'])
-
-
-
   def test_copy_card(self):
     t = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    newCard_id = mb.copy_card(source_card_id=166, destination_collection_id=29, destination_card_name='test_copy_card_{}'.format(t))
+    newCard_id = mb.copy_card(source_card_id=1, destination_collection_id=1, destination_card_name='test_copy_card_{}'.format(t))
     
     # make sure the cards were created in the right collection
     res = mb.get('/api/card/{}'.format(newCard_id))
-    self.assertEqual(res['collection_id'], 29)
+    self.assertEqual(res['collection_id'], 1)
     
     # add to cleanup list
     Metabase_API_Test.cleanup_objects['card'].append(res['id'])
@@ -258,10 +219,10 @@ class Metabase_API_Test(unittest.TestCase):
     t = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
     # shallow copy
-    dup_dashboard_id_shallow = mb.copy_dashboard(source_dashboard_id=35, destination_collection_id=29, postfix='_dup_shallow_{}'.format(t))
+    dup_dashboard_id_shallow = mb.copy_dashboard(source_dashboard_id=1, destination_collection_id=1, postfix='_dup_shallow_{}'.format(t))
 
     # deep copy
-    dup_dashboard_id_deep = mb.copy_dashboard(source_dashboard_id=35, destination_collection_id=29, postfix='_dup_deep_{}'.format(t), deepcopy=True)
+    dup_dashboard_id_deep = mb.copy_dashboard(source_dashboard_id=1, destination_collection_id=1, postfix='_dup_deep_{}'.format(t), deepcopy=True)
     new_collection_id = mb.get_item_id('collection', "test_dashboard_dup_deep_{}'s cards".format(t))
 
     # add to cleanup list
@@ -272,7 +233,7 @@ class Metabase_API_Test(unittest.TestCase):
 
   def test_copy_collection(self):
     t = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    mb.copy_collection(source_collection_id=28, destination_parent_collection_id=1, destination_collection_name='test_copy_collection_{}'.format(t))
+    mb.copy_collection(source_collection_id=3, destination_parent_collection_id=1, destination_collection_name='test_copy_collection_{}'.format(t))
     new_collection_id = mb.get_item_id('collection', 'test_copy_collection_{}'.format(t))
 
     # add to cleanup list
@@ -281,7 +242,7 @@ class Metabase_API_Test(unittest.TestCase):
 
 
   def test_search(self):
-    res = mb.search('test_db2')
+    res = mb.search('test_db')
     self.assertEqual(len(res), 1)
     self.assertEqual(res[0]['model'], 'database')
 
@@ -289,7 +250,7 @@ class Metabase_API_Test(unittest.TestCase):
 
   def test_get_card_data(self):
     # json
-    res = mb.get_card_data(card_id=166)
+    res = mb.get_card_data(card_id=1)
     json_data = [
       {'col1': 'row1 cell1', 'col2': 1},
       {'col1': None, 'col2': 2},
@@ -300,27 +261,27 @@ class Metabase_API_Test(unittest.TestCase):
     self.assertEqual(res, json_data)
 
     # csv
-    res = mb.get_card_data(card_id=166, data_format='csv')
+    res = mb.get_card_data(card_id=1, data_format='csv')
     csv_data = 'col1,col2\nrow1 cell1,1\n,2\nrow3 cell1,\n,\nrow5 cell1,5\n'
     self.assertEqual(res, csv_data)
 
     # filtered data
-    res = mb.get_card_data(card_id=273, parameters=[{"type":"category","value":[1,2],"target":["dimension",["template-tag","test_filter"]]}])
-    filtered_data = [{'col1': 'row1 cell1', 'col2': 1}, {'col1': None, 'col2': 2}]
+    res = mb.get_card_data(card_id=2, parameters=[{"type":"string/=","value":['row1 cell1', 'row3 cell1'],"target":["dimension",["template-tag","test_filter"]]}])
+    filtered_data = [{'col1': 'row1 cell1', 'col2': 1}, {'col1': 'row3 cell1', 'col2': None}]
     self.assertEqual(res, filtered_data)
 
 
 
   def test_clone_card(self):
     # native question
-    res = mb.clone_card(273, 101, 103, new_card_name='test_clone_native', new_card_collection_id=29, return_card=True)
+    res = mb.clone_card(2, 9, 10, new_card_name='test_clone_native', new_card_collection_id=1, return_card=True)
     # simple/custom question
-    res2 = mb.clone_card(274, 101, 103, new_card_name='test_clone_simple1', new_card_collection_id=29, return_card=True)
-    res3 = mb.clone_card(491, 101, 103, new_card_name='test_clone_simple2', new_card_collection_id=29, return_card=True)
-    expected_res3_query = { 'database': 5,
-                            'query': {'source-table': 103,
-                                      'aggregation': [['avg', ['field', 493, None]]],
-                                      'breakout': [['field', 494, None]],
+    res2 = mb.clone_card(3, 9, 10, new_card_name='test_clone_simple1', new_card_collection_id=1, return_card=True)
+    res3 = mb.clone_card(4, 9, 10, new_card_name='test_clone_simple2', new_card_collection_id=1, return_card=True)
+    expected_res3_query = { 'database': 2,
+                            'query': {'source-table': 10,
+                                      'aggregation': [['avg', ['field', 75, None]]],
+                                      'breakout': [['field', 74, None]],
                                       'order-by': [['desc', ['aggregation', 0, None]]]
                                     },
                             'type': 'query'
@@ -333,12 +294,12 @@ class Metabase_API_Test(unittest.TestCase):
 
 
   def test_update_column(self):
-    mb.update_column(params={'semantic_type':'type/City'}, column_id=490)
-    res = mb.get('/api/field/490')['semantic_type']
+    mb.update_column(params={'semantic_type':'type/City'}, column_id=72)
+    res = mb.get('/api/field/72')['semantic_type']
     self.assertEqual(res, 'type/City')
 
-    mb.update_column(params={'semantic_type':'type/Category'}, column_id=490)
-    res = mb.get('/api/field/490')['semantic_type']
+    mb.update_column(params={'semantic_type':'type/Category'}, column_id=72)
+    res = mb.get('/api/field/72')['semantic_type']
     self.assertEqual(res, 'type/Category')
 
 
