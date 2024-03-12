cf445ec1dd83aacb7a71edf3a686f7759ccac1bb
Luis Da Costa <luis@assistiq.ai>
2024-03-12 13:16:01 -0400

    debugs extreme cases + uses logger

--- scripts/test_api/migration/main.py
+++ scripts/test_api/migration/main.py
@@ -4,10 +4,19 @@ Migrates a collection.
 import argparse
 import ast
 
+import logging
+
 from metabase_api.metabase_api import Metabase_API
 from metabase_api.migration import migrate_collection
 
+from metabase_api.utility import logger
+
+_logger = logging.getLogger(__name__)
+
 if __name__ == "__main__":
+    logger.setup()
+    _logger = logging.getLogger(__name__)
+
     parser = argparse.ArgumentParser(
         description="Collection migration",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
@@ -29,14 +38,18 @@ if __name__ == "__main__":
     args = parser.parse_args()
     config = vars(args)
 
-    migrate_collection(
-        metabase_api=Metabase_API(
-            "https://assistiq.metabaseapp.com", "wafa@assistiq.ai", "AssistIQ2023."
-        ),
-        source_collection_id=config["from"],
-        db_target=config["db_target"],
-        parent_collection_id=config["to_parent"],
-        destination_collection_name=config["to"],
-        table_src2dst=config["tables"],
-        new_dashboard_name=config["to"],
-    )
+    try:
+        migrate_collection(
+            metabase_api=Metabase_API(
+                "https://assistiq.metabaseapp.com", "wafa@assistiq.ai", "AssistIQ2023."
+            ),
+            source_collection_id=config["from"],
+            db_target=config["db_target"],
+            parent_collection_id=config["to_parent"],
+            destination_collection_name=config["to"],
+            table_src2dst=config["tables"],
+            new_dashboard_name=config["to"],
+        )
+    except Exception as e:
+        _logger.error(f"Dashboard not properly migrated!: {str(e)}")
+        # todo: delete the destination collection, since it's not valid
