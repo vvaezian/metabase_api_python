e8d22be486de9cbf02814b88f933571bcc29be0a
Vahid Vaezian <vahid.vaezian@gmail.com>
2024-06-01 15:28:19 -0700

    fix unittest of `get_card_data` function

--- tests/test_metabase_api.py
+++ tests/test_metabase_api.py
@@ -278,7 +278,7 @@ class Metabase_API_Test(unittest.TestCase):
 
     # filtered data
     res = mb.get_card_data(card_id=2, parameters=[{"type":"string/=","value":['row1 cell1', 'row3 cell1'],"target":["dimension",["template-tag","test_filter"]]}])
-    filtered_data = [{'col1': 'row1 cell1', 'col2': '1'}, {'col1': 'row3 cell1', 'col2': None}]
+    filtered_data = [{'col1': 'row1 cell1', 'col2': 1}, {'col1': 'row3 cell1', 'col2': None}]
     self.assertEqual(res, filtered_data)
 
 
